5ad5779a6a271765467afd77a4cf0b88e27b6330
Marian <vitalariu.marian93@gmail.com>
2025-01-05 14:45:07 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // TO DO:
+
+        int luna = scanner.nextInt();
+
+        if (luna == 1) {
+            System.out.println("Ianuarie");
+        } else if (luna == 2) {
+            System.out.println("Februarie");
+        } else if (luna == 3) {
+            System.out.println("Martie");
+        } else if (luna == 4) {
+            System.out.println("Aprilie");
+        } else if (luna == 5) {
+            System.out.println("Mai");
+        } else if (luna == 6) {
+            System.out.println("Iunie");
+        } else if (luna == 7) {
+            System.out.println("Iulie");
+        } else if (luna == 8) {
+            System.out.println("August");
+        } else if (luna == 9) {
+            System.out.println("Septembrie");
+        } else if (luna == 10) {
+            System.out.println("Octombrie");
+        } else if (luna == 11) {
+            System.out.println("Noiembrie");
+        } else if (luna == 12) {
+            System.out.println("Decembrie");
+        }
+    }
+}
